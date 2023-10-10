de92a5805a9edf47c2e4db80eac6b51d03439800
ran1001001 <marionlove07@gmail.com>
2023-10-11 07:29:21 +0800

    quick sort done!

--- challenges-guided_project/Learn_Algorithms/Sorting_Algorithms/quick_sort.py
+++ challenges-guided_project/Learn_Algorithms/Sorting_Algorithms/quick_sort.py
@@ -3,13 +3,22 @@ import random
 
 
 def quick_sort(nums, low, high):
-    # ?
+    if low < high:
+        nums, i = partition(nums, low, high)
+        nums = quick_sort(nums, low, i - 1)
+        nums = quick_sort(nums, i + 1, high)
+    return nums
+
 
 def partition(nums, low, high):
-    # ?
-
-
-# don't touch below this line
+    pivot = nums[high]
+    i = low
+    for j in range(low, high):
+        if nums[j] < pivot:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+    nums[i], nums[high] = nums[high], nums[i]
+    return nums, i
 
 
 def benchmark(nums, show_nums):
@@ -22,7 +31,8 @@ def benchmark(nums, show_nums):
     if (end - start) < timeout:
         print(f"test completed in less than {timeout * 1000} milliseconds!")
     else:
-        print(f"test took too long ({(end - start) * 1000} milliseconds). Speed it up!")
+        print(f"""test took too long ({(end - start) * 1000} milliseconds).
+              Speed it up!""")
     print("====================================")
 
 
