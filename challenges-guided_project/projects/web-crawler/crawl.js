d81d273e7f94b2e096928c83fb8653856f5c9614
ran1001001 <marionlove07@gmail.com>
2024-01-13 09:36:45 +0800

    added absolute/relative urls tests

--- challenges-guided_project/projects/web-crawler/crawl.js
+++ challenges-guided_project/projects/web-crawler/crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(urlStr) {
     let url = require('node:url').parse(urlStr);
     let hostname = url.hostname
@@ -9,6 +11,40 @@ function normalizeURL(urlStr) {
     return fullpath
 }
 
+
+const isValidUrl = urlString => {
+    try {
+        return Boolean(new URL(urlString));
+    } catch (e) {
+        return false;
+    }
+}
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    let links = []
+    const doc = new JSDOM(htmlBody)
+    const anchors = doc.window.document.querySelectorAll('a')
+    for (let index = 0; index < anchors.length; index++) {
+        let link = anchors[index].href
+        //handle relative urls
+        if (link.startsWith('/')) {
+            link = baseURL + link
+            links.push(link);
+            continue
+        }
+        //handle absolute urls
+        if (link.endsWith('/')) {
+            link = link.slice(0, -1)
+        }
+        //handle faulty url
+        if (isValidUrl(link)) {
+            links.push(link);
+        }
+    }
+    return links
+};
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
