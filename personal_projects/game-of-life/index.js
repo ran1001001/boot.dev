9298f791c80f02aa711c966393470ff7bf5b9c98
ran1001001 <marionlove07@gmail.com>
2024-01-28 19:10:06 +0800

    added static for htmx

--- personal_projects/game-of-life/index.js
+++ personal_projects/game-of-life/index.js
@@ -2,9 +2,10 @@ const { readFile } = require('fs').promises
 const express = require('express')
 const app = express()
 
+app.use(express.static('static'))
 app.get('/', async (req, res) => {
-    const html = await readFile('./test.html', 'utf-8')
-    res.send(html) 
+    const html = await readFile('./static/game.html', 'utf-8')
+    res.send(html)
 })
 
 app.listen(3000, () => {
